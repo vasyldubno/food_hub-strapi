579c41913d64bd1565e51cd9a1dc5020b2b42a5a
Vasyl Dubno <vasyldubno@gmail.com>
2023-02-18 11:37:42 -0800

    fix

--- config/plugins.js
+++ config/plugins.js
@@ -23,5 +23,17 @@ module.exports = ({ env }) => ({
         delete: {},
       },
     },
+  },
+
+  "vercel-deploy": {
+    enabled: true,
+    config: {
+      deployHook:
+        "https://api.vercel.com/v1/integrations/deploy/prj_RvOhvDWQBkfMZ8Aowq1NozD108Tu/5xZ5Z3kN09",
+      apiToken: "bRBaEUbtqNTPJdYtU4bNDoKq",
+      appFilter: "food-hub-strapi",
+      teamFilter: "your-team-id-on-vercel",
+      roles: ["strapi-super-admin", "strapi-editor", "strapi-author"],
+    }
   }
 });
